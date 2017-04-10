b62fb833c4f8c843139c80a50a9e7c81eb4fbb57
Stephen Wisner <spwisner@gmail.com>
2017-04-10 00:57:14 -0400

    Updated Document and Contact Schema
    - Ensured that controllers and serializers were consistent
    with current schema

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -50,7 +50,10 @@ class ContactsController < OpenReadController
 
   # Only allow a trusted parameter "white list" through.
   def contact_params
-    params.require(:contact).permit(:user_id, :first_name, :last_name, :full_name, :nickname, :contact_name, :job_title, :email, :phone, :website, :notes, :company_ref_id,
-                                    :job_ref_id, :document_ref_id)
+    params.require(:contact).permit(:user_id, :first_name, :last_name,
+                                    :full_name, :nickname, :contact_name,
+                                    :job_title, :email, :phone, :website,
+                                    :notes, :company_ref_id, :job_ref_id,
+                                    :document_ref_id, :company_name)
   end
 end
--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -1,8 +1,7 @@
 class DocumentSerializer < ActiveModel::Serializer
   attributes :id, :doctype, :doctext, :docsubject, :docurl, :company_ref_id,
              :company_name, :contact_ref_id, :contact_ref_name, :docdate,
-             :docsubject, :job_ref_id, :contact_ref_id, :doctypeother,
-             :doctypeshow, :editable
+             :docsubject, :job_ref_id, :contact_ref_id, :editable
 
   def editable
     scope == object.user
